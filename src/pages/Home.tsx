0425381419aa13de6268cab9495315b13f03452e
Gabriel Lobo <gabrielrblgbi@gmail.com>
2022-11-29 02:27:15 -0300

    feat: put all pages/components on main component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,64 @@
 import { useState } from 'react';
 
+// * MUI
+import { Box, Grid } from '@mui/material';
+
 // * Components
-import { Input } from '../components/Input';
-import { Carousel } from '../components/Carousel';
-import { Button } from '../components/Button';
+import { Navbar } from './Navbar';
+import { Header } from './Header';
+import { Content } from './Content';
+import { Modal } from '../components/Modal';
 
 // * Utils
-import { useQuery } from 'react-query'
+import { useQuery } from 'react-query';
 
 // * Models
-import { GIF } from '../models'
+import { GIF } from '../models';
 
 // * Services
-import { fetchSearch } from '../services'
+import { fetchSearch } from '../services';
 
 export const Home = () => {
   const [search, setSearch] = useState('')
   const [gifs, setGifs] = useState<GIF[]>([])
+  const [selectedGif, setSelectedGif] = useState<GIF | null>(null)
 
   const searchQuery = useQuery({
     queryKey: ['search', search],
     queryFn: () => fetchSearch({ q: search }),
-    enabled: search !== '',
+    enabled: false,
     onSuccess: (data) => setGifs(data.data)
   })
 
+  const onSubmit = () => {
+    searchQuery.refetch()
+  }
+
   return (
-    <div>
-      <Input label='Search' value={search} name='q' onChange={(value: string) => setSearch(value)} />
+    <>
+      <Navbar />
 
-      <Button  />
+      <Box sx={{ height: 'calc(100vh - 64px)' }}>
+        <Grid container justifyContent="center" alignItems="center" sx={{ height: '100%' }}>
+          <Grid item xs={12} md={12} lg={12} mx={12}>
+            <Header
+              search={search}
+              setSearch={setSearch}
+              onSubmit={onSubmit}
+            />
+          </Grid>
 
-      <Carousel gifs={gifs} loading={searchQuery.isLoading} />
-    </div>
+          <Grid item xs={12} md={12} lg={12}>
+            <Content
+              gifs={gifs}
+              loading={searchQuery.isLoading}
+              setSelectedGif={setSelectedGif}
+            />
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Modal gif={selectedGif} setSelectedGif={setSelectedGif} />
+    </>
   )
 }
