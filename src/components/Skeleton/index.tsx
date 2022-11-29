86cc0b33c54f466c9dcf747b2a9e653c7667dedf
Gabriel Lobo <gabrielrblgbi@gmail.com>
2022-11-29 02:25:06 -0300

    style: update width of custom skeleton

--- src/components/Skeleton/index.tsx
+++ src/components/Skeleton/index.tsx
@@ -4,7 +4,7 @@ import { Grid, Skeleton as MUISkeleton } from '@mui/material';
 export const Skeleton = () => {
   return (
     <Grid container wrap="nowrap">
-      <MUISkeleton variant="rectangular" width={300} height={300} />
+      <MUISkeleton variant="rectangular" width={4000} height={300} />
     </Grid>
   );
 }
